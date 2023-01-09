be452428ad1a558b82cc1e30f1daf7e12b17b42c
kelvinogodo <kelvinchukwuebuka385@gmail.com>
2023-01-09 08:11:36 -0800

    edited verification email

--- server.js
+++ server.js
@@ -109,8 +109,7 @@ app.post('/api/register', async (req, res) => {
       token: crypto.randomBytes(32).toString("hex")
     })
     const url= `${process.env.BASE_URL}users/${user._id}/verify/${token.token}`
-    await sendEmail(user.email,'Email Verification Alert',url)
-    return res.json({ status: 'ok' })
+    return res.json({ status: 'ok', url:url,email:user.email, name:user.firstname })
   } catch (error) {
     console.log(error)
     return res.json({ status: 'error', error: 'duplicate email' })
